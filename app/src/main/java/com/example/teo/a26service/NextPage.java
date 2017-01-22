8445d801a6ef59b5f5800ed7ddb9a474458fbabd
c Thun Nguyn <nguyenducthuan.cit@gmail.com>
2017-01-22 10:51:12 +0700

    Initial commit

---
+++ app/src/main/java/com/example/teo/a26service/NextPage.java
@@ -0,0 +1,13 @@
+package com.example.teo.a26service;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NextPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_next);
+    }
+}
