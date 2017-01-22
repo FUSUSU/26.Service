db6eb4e84f1c9af8cb2e8b24016ab4a766d9b68b
c Thun Nguyn <nguyenducthuan.cit@gmail.com>
2017-01-22 13:35:50 +0700

    Service

--- app/src/main/java/com/example/teo/a26service/MainActivity.java
+++ app/src/main/java/com/example/teo/a26service/MainActivity.java
@@ -1,13 +1,21 @@
 package com.example.teo.a26service;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity implements View.OnClickListener{
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
