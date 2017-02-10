94249164c2dcebda4aee1adf764115bf9fb1a8fe
c Thun Nguyn <nguyenducthuan.cit@gmail.com>
2017-02-10 17:09:08 +0700

    Finished

--- app/src/main/java/com/example/teo/a26service/MainActivity.java
+++ app/src/main/java/com/example/teo/a26service/MainActivity.java
@@ -1,21 +1,45 @@
 package com.example.teo.a26service;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends Activity implements View.OnClickListener{
-
+    //Khai báo
+    Button buttonStart, buttonStop, buttonNext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Ánh xạ
+        buttonStart = (Button) findViewById(R.id.buttonStart);
+        buttonStop = (Button) findViewById(R.id.buttonStop);
+        buttonNext = (Button) findViewById(R.id.buttonNext);
+
+        //Bất chế độ lắng nghe.
+        buttonStart.setOnClickListener(this);
+        buttonStop.setOnClickListener(this);
+        buttonNext.setOnClickListener(this);
     }
 
     @Override
-    public void onClick(View v) {
-
+    public void onClick(View src) {
+        switch (src.getId()){
+            case R.id.buttonStart:
+                startService(new Intent(this, MyService.class)); //Bắt đầu service
+                break;
+            case R.id.buttonStop:
+                stopService(new Intent(this, MyService.class)); //Kết thức service
+                break;
+            case R.id.buttonNext:
+                Intent intent = new Intent(this, NextPage.class); //Chuyển trang service vẫn chạy ngầm
+                startActivity(intent);
+                break;
+        }
     }
 }
--- app/src/main/java/com/example/teo/a26service/MyService.java
+++ app/src/main/java/com/example/teo/a26service/MyService.java
@@ -20,20 +20,24 @@ public class MyService extends Service{
         return null;
     }
 
+    //Tạo
     @Override
     public void onCreate() {
         Toast.makeText(this, "Service Created", Toast.LENGTH_LONG).show();
 
         myPlayer = MediaPlayer.create(this, R.raw.didetrove);
         myPlayer.setLooping(false); //Set looping
+        //Chế độ không lặp nhạc
     }
 
+    //Bắt đầuu
     @Override
     public void onStart(Intent intent, int startId) {
         Toast.makeText(this, "Service Started", Toast.LENGTH_LONG).show();
         myPlayer.start();
     }
 
+    //Kết thúc
     @Override
     public void onDestroy() {
         Toast.makeText(this, "Service Stopped", Toast.LENGTH_LONG).show();
